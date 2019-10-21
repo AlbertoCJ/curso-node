4dea60f0239cc05db310625bfa768fd784519de7
albertoCJ <albertocj88@gmail.com>
2019-10-21 20:03:11 +0200

    Autenticación de usuario - JWT

--- 07-restserver/README.md
+++ 07-restserver/README.md
@@ -5,6 +5,19 @@ npm i -g nodemon
 npm i express --save
 npm i body-parser --save 
 npm i mongoose --save
-npm install mongoose-unique-validator --save
+npm i mongoose-unique-validator --save
 npm i bcrypt --save
-npm install underscore --save
+npm i underscore --save
+npm i jsonwebtoken --save
+
+
+tip postman:
+En seccion test (Params, Headers, etc)
+
+let resp = pm.response.json();
+if (resp.ok) {
+    let token = resp.token;
+    pm.environment.set("token", token);   
+} else {
+    console.log('No se actualizó el token');
+}
--- 07-restserver/server/routes/usuario.js
+++ 07-restserver/server/routes/usuario.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+const app = express();
 
-const app = express()
+app.get('/usuario', verificaToken, (req, res) => {
 
-app.get('/usuario', function(req, res) {
+    /* return res.json({
+        usuario: req.usuario
+    }); */
 
     // Paginación
     let desde = req.query.desde || 0;
@@ -36,7 +40,7 @@ app.get('/usuario', function(req, res) {
 
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -61,7 +65,7 @@ app.post('/usuario', function(req, res) {
     });
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -81,7 +85,7 @@ app.put('/usuario/:id', function(req, res) {
     })
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
     /* Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
